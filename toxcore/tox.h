802ca41fffebfcb7eccb09655b4fe83ab88cd977
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-08-07 15:04:30 -0700

    Minor Version increment for Mdevice

--- toxcore/tox.h
+++ toxcore/tox.h
@@ -150,7 +150,7 @@ typedef struct Tox Tox;
  * breaking the API or ABI. Set to 0 when the major version number is
  * incremented.
  */
-#define TOX_VERSION_MINOR               0u
+#define TOX_VERSION_MINOR               1u
 
 /**
  * The patch or revision number. Incremented when bugfixes are applied without
